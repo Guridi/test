75306738412021f702aa38e33f99770bae5a8dde
Guridi <39349521+Guridi@users.noreply.github.com>
2023-03-20 00:05:45 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,45 @@
+basic.forever(function () {
+    basic.showString("Hola")
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # . # .
+        . # . # .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        . # . . .
+        . # . . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        # . . . #
+        # # . # #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
